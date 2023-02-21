d2cbd5f042ffacf4ae5193c239baeeb6777f2be4
Benjamin Gajski <benjamin.gajski@gmail.com>
2023-02-21 23:00:36 +0100

    Commit

--- Models/LanEventDTO.cs
+++ Models/LanEventDTO.cs
@@ -8,15 +8,15 @@ namespace Models
         public int EventId { get; set; }
         [Required(ErrorMessage = "Potrebno je unijeti naziv turnira")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Potrebno je unijeti broj timova")]
+        [Range(2, int.MaxValue, ErrorMessage = "Potrebno je unijeti broj timova te mimimalan broj dopušten može biti 2 tima")]
         public int Teams { get; set; }
-        [Required(ErrorMessage = "Potrebno je unijeti iznos nagradnog fonda u Eurima")]
+        [Range(30, int.MaxValue, ErrorMessage = "Potrebno je unijeti iznos nagradnog fonda u Eurima te minimalan iznos može biti 20 Eura")]
         public int Reward { get; set; }
         [Required(ErrorMessage = "Potrebno je unijeti datum početka turnira")]
         public DateTime Starting { get; set; }
         [Required(ErrorMessage = "Potrebno je unijeti datum kraja turnira")]
         public DateTime Ending { get; set; }
-        [Required(ErrorMessage = "Potrebno je unijeti cijenu registracije po timu")]
+        [Range(1, int.MaxValue, ErrorMessage = "Potrebno je unijeti cijenu registracije po timu u Eurima te minimalan iznos može biti 20 Eura")]
         public int Price { get; set; }
         public int? LanGameId { get; set; }
         public LanGameDTO LanGameDTO { get; set; }
